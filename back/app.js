d5078235b8c3f998d34817a97f424b9d59f17133
Chloé T. <chloe.thomas14@gmail.com>
2024-03-19 15:35:54 +0100

    test

--- back/app.js
+++ back/app.js
@@ -11,83 +11,9 @@ const pool = new Pool({
     connectionString:process.env.SUPABASE_URI
 });
 
-/*
-Connexion database POSTGRESQL
-*/
-// const { Pool } = require('pg');
-// const pool = new Pool({
-//         user: "postgres",
-//         host: "localhost",
-//         database: "crochet_library",
-//         password: "T0d&Capsul3!",
-//         port: 5432,
-//     });  
-
-
 const app = express();
 app.use(express.urlencoded({extended:true}));
 
-// const crochetLibrary = [
-//     {
-//         "_id":1,
-//         "name":"Taro le dino",
-//         "category":"Amigurumi",
-//         "pattern" : {
-//             "name":"taro le dino",
-//             "origin":"cultura"
-//         },
-//         "technical":{
-//             "hookSize":3,
-//             "stitch":["mailles serées", "cercle magique"]
-//         },
-//         "wool":{
-//             "name":"Ricorumi",
-//             "brand":"Ricorumi",
-//             "type":"coton"
-//         },
-//         "notes":"facile à faire"
-//     },
-//     {
-//         "_id":2,
-//         "name":"Hochet citron",
-//         "category":"Amigurumi",
-//         "pattern" : {
-//             "name":"hochet citron",
-//             "origin":"hobbi"
-//         },
-//         "technical":{
-//             "hookSize":3,
-//             "stitch":["mailles serées", "cercle magique"]
-//         },
-//         "wool":{
-//             "name":"Ricorumi",
-//             "brand":"Ricorumi",
-//             "type":"coton"
-//         },
-//         "notes":"facile à faire"
-//     },
-//     {
-//         "_id":3,
-//         "name":"Pochette écouteurs",
-//         "category":"Accessoire",
-//         "pattern" : {
-//             "name":"pochette écouteurs",
-//             "origin":"hibouchoucaillou"
-//         },
-//         "technical":{
-//             "hookSize":3,
-//             "stitch":["granny square"]
-//         },
-//         "wool":{
-//             "name":"",
-//             "brand":"",
-//             "type":"coton"
-//         },
-//         "notes":"facile à faire et très pratique"
-//     },
-// ];
-
-// const queryDataTest = "SELECT project.id, project.name,project.category, project.photo, project.year, project.month, project.customer, project.size, project.hook_number, project.notes, pattern.name, pattern.source, pattern.link, pattern.file FROM project INNER JOIN pattern_project_relation ON project.id = pattern_project_relation.project_id INNER JOIN pattern ON pattern_project_relation.pattern_id = pattern.id"
 
 app.get("/api/all", async (req, res) => {
     try {
