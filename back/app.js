1a15b8621a73a6b1fd922fe265c361caad183d33
Chloé T. <chloe.thomas14@gmail.com>
2024-03-20 18:55:01 +0100

    fix cors

--- back/app.js
+++ back/app.js
@@ -5,7 +5,6 @@ const getDataByProject = require('./query').getDataByProject
 require('dotenv').config()
 
 const app = express();
-app.use(cors());
 app.use(express.urlencoded({extended:true}));
 
 /*
@@ -15,6 +14,12 @@ const pool = new Pool({
     connectionString:process.env.SUPABASE_URI
 });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.get("/api/all", async (req, res) => {
     try {
