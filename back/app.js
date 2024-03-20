910d309a306cabbdca70ec6486aaeb4c62ccc3e5
Chloé T. <chloe.thomas14@gmail.com>
2024-03-20 18:50:48 +0100

    cors fix

--- back/app.js
+++ back/app.js
@@ -5,6 +5,7 @@ const getDataByProject = require('./query').getDataByProject
 require('dotenv').config()
 
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 
 /*
